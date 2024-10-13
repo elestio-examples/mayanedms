c46be4f93835045d0ce8dff73c9ce7dc5a4a0473
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-10-13 13:23:42 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -3,5 +3,3 @@ set -o allexport; source .env; set +o allexport;
 
 echo "Waiting for software to be ready ..."
 sleep 45s;
-
-docker-compose exec -T healthchecks sh -c "/opt/healthchecks/manage.py create_admin"
